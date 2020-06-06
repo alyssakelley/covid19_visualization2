a74295ad587e250cae4af14475714283b53592c9
alyssakelley <abkelley97@gmail.com>
2020-06-05 22:57:50 -0700

    pushed for gitpages

---
+++ build/precache-manifest.54e8cf471582bcfba2cd9efc36610495.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4bcf79b07df0dc9f2968f34a1374aeef",
+    "url": "/covid19_visualization/index.html"
+  },
+  {
+    "revision": "0b5b8bbb538a679908ee",
+    "url": "/covid19_visualization/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "4597c1835ec6154eba69",
+    "url": "/covid19_visualization/static/css/main.6d35838e.chunk.css"
+  },
+  {
+    "revision": "0b5b8bbb538a679908ee",
+    "url": "/covid19_visualization/static/js/2.33142377.chunk.js"
+  },
+  {
+    "revision": "b1af44c7d782d8cd3b71e08e7c324f6e",
+    "url": "/covid19_visualization/static/js/2.33142377.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4597c1835ec6154eba69",
+    "url": "/covid19_visualization/static/js/main.06dfec07.chunk.js"
+  },
+  {
+    "revision": "5102fa159b8272971a3c",
+    "url": "/covid19_visualization/static/js/runtime-main.41c7dd97.js"
+  },
+  {
+    "revision": "5255d57ebe9388ea934b341ec84f5cff",
+    "url": "/covid19_visualization/static/media/covid19_image2.5255d57e.png"
+  },
+  {
+    "revision": "5c1adb944b6f28a6fa859c8d3511a8e5",
+    "url": "/covid19_visualization/static/media/sym_breathing.5c1adb94.png"
+  },
+  {
+    "revision": "99315a68712fb7d7dc377811dd9de558",
+    "url": "/covid19_visualization/static/media/sym_cough.99315a68.png"
+  },
+  {
+    "revision": "560e5289680049d8788a72f7d6b0d351",
+    "url": "/covid19_visualization/static/media/sym_fever.560e5289.png"
+  },
+  {
+    "revision": "60a40790dea7b504020c451b260dc013",
+    "url": "/covid19_visualization/static/media/th1_breathing.60a40790.png"
+  },
+  {
+    "revision": "9df417b6bc72409f7193b3c46aba1e37",
+    "url": "/covid19_visualization/static/media/th1_temp.9df417b6.png"
+  },
+  {
+    "revision": "65410af54ff46e06d5a3e6355fb1589b",
+    "url": "/covid19_visualization/static/media/th1_washhands.65410af5.png"
+  }
+]);
